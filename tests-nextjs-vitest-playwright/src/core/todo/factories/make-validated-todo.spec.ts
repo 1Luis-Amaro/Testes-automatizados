112c9e42c114732ebc4f7a05dee8b9bfb1f5fd02
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2026-09-04 12:56:30 -0300

    292 - Mock com vi.spyOn no Vitest: como interceptar e modificar funções (Parte 2)

--- tests-nextjs-vitest-playwright/src/core/todo/factories/make-validated-todo.spec.ts
+++ tests-nextjs-vitest-playwright/src/core/todo/factories/make-validated-todo.spec.ts
@@ -1,10 +1,23 @@
-import * as sanitizeStrMod from "@/utils/sanitize-str"
+import * as sanitizeStrMod from "@/utils/sanitize-str" //estou pegando todo o module de limpeza de caracteres 
+import { makeValidetedTodo } from "./make-validated-todo" // Importa a função que valida e cria uma nova tarefa
 
-describe('makeValidatedTodo (unit)', () => {
+describe('makeValidatedTodo (unit)', () => { 
     test('deve chamar a função sanitizeStr com o valor correto', () => {
-        //Arrange 
-        const description = 'abcd'
-        const sanitizeStrSpy = vi.spyOn(sanitizeStrMod, 'sanitizeStr')
+        //Arrange  (Preparação)
+        const description = 'abcd' //// Descrição que será passada para a função de limpeza
+        const sanitizeStrSpy = vi //chamo o vitest para pegar suas funções
+        .spyOn(sanitizeStrMod, 'sanitizeStr') // Crio um "espião" (spy) para monitorar a função sanitizeStr (ver se foi chamada, quantas vezes, com quais argumentos)
+        .mockReturnValue(description)  // Em vez de executar a função real, eu "mocko" (simulo) ela: defino que, quando sanitizeStr for chamada, ela vai retornar o valor que eu coloquei aqui (description), sem executar a lógica original
+        
+        //Act (Ação) 
+        makeValidetedTodo(description)// Chamo a função que valida a tarefa
+
+        // Assert 
+        expect(sanitizeStrSpy).toHaveBeenCalledExactlyOnceWith(description) // Verifico se a função sanitizeStr foi chamada EXATAMENTE 1 VEZ e com a descrição correta
+        expect(sanitizeStrSpy).toHaveBeenCalledTimes(1) // Verifico se a função sanitizeStr foi chamada 1 vez (redundante, mas seguro)
+        expect(sanitizeStrSpy).toHaveBeenCalledWith(description) // Verifico se a função sanitizeStr foi chamada com a descrição correta
+
+
     })
 
     test('deve chamar a validatedDescription com o retorno do sanitizeStr', () => {})

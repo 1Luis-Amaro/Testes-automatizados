944971de680edcb738d9791260be2d32ac29d8ca
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2026-09-15 18:58:12 -0300

    303 - Teste de integração do repository com Drizzle ORM - findAll + Helper

---
+++ tests-nextjs-vitest-playwright/src/core/__tests__/utils/make-test-todo-repository.ts
@@ -0,0 +1,40 @@
+import { DrizzleTodoRepository } from "@/core/todo/repositores/drizzle-todo.repository";
+import { Todo } from "@/core/todo/schemas/todo.contract";
+import { drizzleDatabase } from "@/db/drizzle";
+import { eq } from "drizzle-orm";
+
+export async function makeTestTodoRepository() {
+    const { db, todoTable } = drizzleDatabase
+    const repository = new DrizzleTodoRepository(db)
+    const insertTodoDb = () => db.insert(todoTable)
+    const deleteTodoNoWhere = () => db.delete(todoTable) 
+    const deleteTodoDb = (id: string) => { db.delete(todoTable).where(eq(todoTable.id, id)) }
+
+    return {
+        repository,
+        insertTodoDb,
+        deleteTodoNoWhere,
+        deleteTodoDb
+    }
+
+}
+
+export const insertTestTodos = async () => {
+    const { insertTodoDb } = await makeTestTodoRepository()
+    const todos = makeTestTodos()
+
+    await insertTodoDb().values(todos);
+
+    return todos;
+}
+
+export const makeTestTodos = () => {
+    return Array.from({ length: 5 }).map((_, index) => {
+        const newTodo = {
+            id: index.toString(),
+            description: `Todo ${index}`,
+            createdAt: `date ${index}`,
+        }
+        return newTodo
+    })
+}
--- tests-nextjs-vitest-playwright/src/core/todo/repositores/drizzle-todo.repository.test.ts
+++ tests-nextjs-vitest-playwright/src/core/todo/repositores/drizzle-todo.repository.test.ts
@@ -1,26 +1,42 @@
 import { drizzleDatabase } from "@/db/drizzle"
 import { DrizzleTodoRepository } from "./drizzle-todo.repository"
+import { insertTestTodos, makeTestTodoRepository } from "@/core/__tests__/utils/make-test-todo-repository"
 
-describe('DrizzleTodoRepository (integration', () => {
+
+describe('DrizzleTodoRepository (integration)', () => {
+    beforeEach(async () => {
+        const { deleteTodoNoWhere } = await makeTestTodoRepository();
+        await deleteTodoNoWhere();
+    });
     describe('findAll', () => {
-        test('deve retornar um array vazio se a tabela estiver limpa', async() => {
-            const repository = new DrizzleTodoRepository(drizzleDatabase.db)
-            expect(await repository.findAll()).toStrictEqual([])
-            expect(await repository.findAll()).toHaveLength(1)
-        })
-        test('deve retornar todos os TODOs em ordem decrescente', async() => {})
-
-        describe('create', () => {
-            test('cria um TODO se os dados estão validos', async() => {})
-            test('falha se houver uma descrição igual na tabela', async() => {})
-            test('falha se houver uma ID igual na tabela', async() => {})
-        })
-
-        describe('delete', () => {
-            test('apaga um todo se ele existir', async () => {})
-            test('falha ao apagar se o todo não existir', async () => {})
+        test('deve retornar um array vazio se a tabela estiver limpa', async () => {
+            const { repository } = await makeTestTodoRepository();
+            const result = await repository.findAll();
+            expect(result).toStrictEqual([]);
+            expect(result).toHaveLength(0);
+        });
+        test('deve retornar todos os TODOs em ordem decrescente', async () => {
+            const { repository } = await makeTestTodoRepository();
+            await insertTestTodos();
+            const result = await repository.findAll();
+            expect(result[0].createdAt).toBe('date 4');
+            expect(result[1].createdAt).toBe('date 3');
+            expect(result[2].createdAt).toBe('date 2');
+            expect(result[3].createdAt).toBe('date 1');
+            expect(result[4].createdAt).toBe('date 0');
         })
     })
 
-    
+    describe('create', () => {
+        test('cria um TODO se os dados estão validos', async () => { })
+        test('falha se houver uma descrição igual na tabela', async () => { })
+        test('falha se houver uma ID igual na tabela', async () => { })
+    })
+
+    describe('delete', () => {
+        test('apaga um todo se ele existir', async () => { })
+        test('falha ao apagar se o todo não existir', async () => { })
+    })
 })
+
+

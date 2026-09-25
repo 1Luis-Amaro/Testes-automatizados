d9fb9a4fefa6f260804be73e01e04503a03b142a
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2026-09-25 12:07:43 -0300

    313 - Escrevendo o primeiro teste com a React Testing Library e Vitest no Next.js

--- tests-nextjs-vitest-playwright/src/components/Button/Button.spec.tsx
+++ tests-nextjs-vitest-playwright/src/components/Button/Button.spec.tsx
@@ -1,12 +1,19 @@
-import { render } from "@testing-library/react";
-import { Button } from ".";
+import { render, screen } from "@testing-library/react"; // Importa as funções render e screen da Testing Library
+import { Button } from "."; // Importa o componente Button
 
-describe('<Button />', () => {
-  describe('props padrão e JSX', () => {
-    test('deve renderizar o botão com props padrão (apenas com children)', async () => {
-      const r = render(<Button>Enviar formulário</Button>);
+describe('<Button />', () => { //descrição do que irei testar, nesse caso vai ser meu componente Button
+  describe('props padrão e JSX', () => { /// Sub-descrição: testa as props padrão e o JSX do botão 
+    test('deve renderizar o botão com props padrão (apenas com children)', async () => { //o que o teste ira fazer
+      render(<Button>Enviar formulário</Button>); // Renderiza o componente Button no DOM virtual com o texto "Enviar formulário"
 
-      r.debug()
+      const button = screen.getByRole('button', { // Busca o elemento <button> no DOM pelo seu papel (role)
+        name: /enviar formulário/i //// E pelo texto (usando regex para ignorar maiúsculas/minúsculas)
+      })
+
+      expect(button).toBeInTheDocument() // Espero que o elemento exista no DOM
+      expect(button).toHaveClass('bg-blue-600 hover:bg-blue-700 text-blue-100')  // Espero que o elemento tenha as classes da variante 'default'
+      expect(button).toHaveClass( // Espero que o elemento tenha as classes do tamanho 'md'
+        'text-base/tight py-2 px-4 rounded-md [&_svg]:w-4 [&_svg]:h-4 gap-2')
     });
 
     // test('verifica se as propriedades padrão do JSX funcionam corretamente', async () => {});

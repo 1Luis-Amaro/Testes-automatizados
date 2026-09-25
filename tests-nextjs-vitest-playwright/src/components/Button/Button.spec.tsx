54f3d3e137a0223dea0b7bd236ff79e9f636f164
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2026-09-25 15:33:18 -0300

    315 - Testando os sizes do botão com RTL, userEvent, vi.fn() e Vitest no Next.js

--- tests-nextjs-vitest-playwright/src/components/Button/Button.spec.tsx
+++ tests-nextjs-vitest-playwright/src/components/Button/Button.spec.tsx
@@ -1,3 +1,6 @@
+//OS SELETORES USADOS NESSE TESTE FORAM APENAS COMO EXEMPLO
+//TENTAR SEMPRE USAR A ORDEM INDICA PELA ANOTAÇÕES 
+
 import { render, screen } from '@testing-library/react'; // Importa as funções render e screen da Testing Library
 import { Button } from '.'; // Importa o componente Button
 import { userEvent } from '@testing-library/user-event';
@@ -7,6 +10,12 @@ const VARIANT_DANGER_CLASSES = 'bg-red-600 hover:bg-red-700 text-red-100';
 const VARIANT_GHOST_CLASSES = 'bg-slate-300 hover:bg-slate-400 text-slate-950';
 const SIZE_DEFAULT_CLASSES =
   'text-base/tight py-2 px-4 rounded-md [&_svg]:w-4 [&_svg]:h-4 gap-2';
+
+const SIZE_SM_CLASSES = 'text-xs/tight py-1 px-2 rounded-sm [&_svg]:w-3 [&_svg]:h-3 gap-1';
+const SIZE_MD_CLASSES = 'text-base/tight py-2 px-4 rounded-md [&_svg]:w-4 [&_svg]:h-4 gap-2'
+const SIZE_LG_CLASSES = 'text-lg/tight py-4 px-6 rounded-lg [&_svg]:w-5 [&_svg]:h-5 gap-3'
+const DISABLED_CLASSES = 'disabled:bg-slate-200 disabled:text-slate-400 disabled:cursor-not-allowed'// Cursor não permitido quando desabilitado
+
 
 describe('<Button />', () => {
   //descrição do que irei testar, nesse caso vai ser meu componente Button
@@ -77,15 +86,49 @@ describe('<Button />', () => {
       });
     });
 
-    // describe('size (tamanhos)', () => {
-    //   test('tamanho sm deve ser menor', async () => {});
+    describe('size (tamanhos)', () => { // Descrição desse novo trecho de testes (variantes de tamanhos)
+      test('tamanho sm deve ser menor', async () => {
+        render(<Button size="sm" title='o botão'>{ /*Renderiza o componente Button no DOM virtual com o texto "Enviar formulário"/*/}
+          Enviar formulário
+        </Button>,
+      ); 
 
-    //   test('tamanho md deve ser médio', async () => {});
+      const button = screen.getByTitle('o botão'); //pego o meu botão pelo title dele 
+      expect(button).toHaveClass(SIZE_SM_CLASSES); // Espero que o elemento tenha as classes do tamanho 'sm'
+      });
 
-    //   test('tamanho lg deve ser grande', async () => {});
-    // });
+      test('tamanho md deve ser médio', async () => {
 
-    // describe('disabled', () => {
-    //   test('classes para estado desativado estão corretas', async () => {});
-    // });
+        render(<Button size="md" title='o botão'>{ /*Renderiza o componente Button no DOM virtual com o texto "Enviar formulário"/*/}
+          Enviar formulário
+        </Button>,
+      ); 
+
+      const button = screen.getByTitle('o botão'); //pego o meu botão pelo title dele 
+      expect(button).toHaveClass(SIZE_MD_CLASSES); // Espero que o elemento tenha as classes do tamanho 'md'
+      });
+
+      test('tamanho lg deve ser grande', async () => {
+         render(<Button size="lg" title='o botão'>{ /*Renderiza o componente Button no DOM virtual com o texto "Enviar formulário"/*/}
+          Enviar formulário
+        </Button>,
+      ); 
+
+      const button = screen.getByTitle('o botão'); //pego o meu botão pelo title dele 
+      expect(button).toHaveClass(SIZE_LG_CLASSES); // Espero que o elemento tenha as classes do tamanho 'lg'
+      });
+    });
+
+    describe('disabled', () => { // Descrição desse novo trecho de testes (verificação de botão desativado)
+      test('classes para estado desativado estão corretas', async () => {
+        render(<Button disabled>{ /*Renderiza o componente Button no DOM virtual com o texto "Enviar formulário"/*/}
+          Enviar formulário
+        </Button>,
+      ); 
+
+      const button = screen.getByRole('button', {name: /enviar formulário/i }); //pego o meu botão pela role dele, e pelo name que tem dentro dele
+      expect(button).toHaveClass(DISABLED_CLASSES); // Espero que o elemento tenha a classe de botão desabilitado 
+      expect(button).toBeDisabled() //espero também que esse botão esteja desabilitado
+      });
+    });
   });
